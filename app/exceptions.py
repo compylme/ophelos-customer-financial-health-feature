4fb25e2c189d8ac42b6b6174db37f2153b1a851c
compylme <adthompson10@gmail.com>
2026-07-16 14:06:26 +0100

    exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,20 @@
+class DomainError(Exception):
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(message)
+
+
+class SnapshotAlreadyExists(DomainError):
+    pass
+
+
+class SnapshotNotFound(DomainError):
+    pass
+
+
+class InvalidFinancialItems(DomainError):
+    pass
+
+
+class InvalidHistoryLimit(DomainError):
+    pass
